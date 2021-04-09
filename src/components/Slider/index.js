31b00289b23aa7fbc5491e7000e496f0fb8e0ad0
DamianStrz <damian.strzalkowski@o2.pl>
2021-04-10 00:37:55 +0200

    Praca nad komponentem ze specjalnościami. Na ten moment renderuje się dodatkowy komponent pobierający dane ze stanu. Dane nowego komponentu powinny być inne w zależności od klikniętego kafelka ze specjalizacją.

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const Slider = () => (
-    <div className='container slider'>
-        <h1 className='slider-title'>Specjalności na kierunku Ekonomia on-line</h1>
-        <div className='slider-boxes'>
-            <div className='slider-boxes__box box1'>
-                <span className='slider-boxes__text'>Rachunkowość i finanse</span>
-            </div>
-            <div className='slider-boxes__box box2'>
-                <span className='slider-boxes__text'>Bankowość</span>
-            </div>
-            <div className='slider-boxes__box box3'>
-                <span className='slider-boxes__text'>Przedsiębiorczość</span>
-            </div>
+const Slider = () => {
 
+    const [slideData, setSlideData] = useState({
+        isActive: false,
+        title: 'Kierunek ekonomia, specjalność: Rachunkowość i finanse',
+        imgTitle: "Rachunkowość i finanse",
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea id molestias nostrum quidem rerum sequi suscipit veritatis? Cum iure, quam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea id molestias nostrum quidem rerum sequi suscipit veritatis? Cum iure, quam?'
+    })
+
+    const handleColorChange = (e) => {
+        setSlideData({...slideData, isActive: !slideData.isActive})
+
+        e.stopPropagation();
+    }
+
+
+    return(
+
+        <div className='container slider'>
+            <h1 className='slider-title'>Specjalności na kierunku Ekonomia on-line</h1>
+            <div className='slider-boxes'>
+                <div className='slider-boxes__box box1' onClick={handleColorChange}>
+                    <span className='slider-boxes__text'>Rachunkowość i finanse</span>
+                </div>
+                <div className='slider-boxes__box box2'>
+                    <span className='slider-boxes__text'>Bankowość</span>
+                </div>
+                <div className='slider-boxes__box box3'>
+                    <span className='slider-boxes__text'>Przedsiębiorczość</span>
+                </div>
+            </div>
+            {slideData.isActive && <SlideContent isActive={slideData.isActive} slideData={slideData}/>}
         </div>
+    )
+}
 
-    </div>
-)
+const SlideContent = ({slideData}) => {
+
+
+    return(
+        <div className='slide' >
+            <div className='slide-content'>
+                <div className='slide-content__img'>{slideData.imgTitle}</div>
+                <div>
+                    <h3 className='slide-content__title'>{slideData.title}</h3>
+                    <p className='slide-content__text'>{slideData.text}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default Slider;
