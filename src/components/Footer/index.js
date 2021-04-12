4f9badeda0175c223b818b30dd4ee9754e3c6764
DamianStrz <damian.strzalkowski@o2.pl>
2021-04-12 22:47:58 +0200

    Dodanie treści właściwych do komponentu z prezentacją kierunków oraz dodatkowe elementy stylowania. Dodana treść właściwa do stopki oraz stylowanie.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Footer = () => (
-    <footer className='container footer'>
-        <div className='footer-content'>Wyższa Szkoła Finansów i Informatyki im. prof. J. Chechlińskiego</div>
-    </footer>
-)
+import logo from '../../assets/logo-wsfi-białe-bez-tła.png'
+
+const Footer = () => {
+
+    return (
+        <footer className='container footer'>
+            <div className='footer-content'>
+                <p className='footer-content__left'>tel.: 42 639 91 10, 501 24 87 81</p>
+                <img className='footer-content__logo' src={logo} alt="logo"/>
+                <p className='footer-content__right'> e-mail: dziekanat@wsfi.pl</p>
+            </div>
+        </footer>
+    )
+}
 
 export default Footer;
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -5,13 +5,22 @@ const Slider = () => {
     const data = {
         titleBoxOne: 'Kierunek ekonomia, specjalność: Rachunkowość i finanse',
         imgTitleBoxOne: "Rachunkowość i finanse",
-        textBoxOne: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea id molestias nostrum quidem rerum sequi suscipit veritatis? Cum iure, quam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea id molestias nostrum quidem rerum sequi suscipit veritatis? Cum iure, quam?',
+        textBoxOne: 'Specjalność ta przeznaczona jest dla osób, które w przyszłości planują swoją karierę m.in. w działach księgowości, finansowych, na stanowiskach menedżerskich oraz dla prowadzących własną działalność biznesową. Na danej specjalności student uczy się jak uzyskać odpowiednie dane opisujące sytuację przedsiębiorstwa, jak je analizować oraz nabywa umiejętności interpretowania otrzymanych wyników. Daje to podstawy do podejmowania trafnych decyzji biznesowych. Na ostatnim roku studiów do wyboru są dwie specjalizacje: Kadry i płace oraz Podatki w przedsiębiorstwie.',
+        linkBoxOne: 'https://wsfi.edu.pl/files/rachunkowosc_i_finanse_program.pdf',
+        linkBoxOneTitle: 'Program studiów: ',
+        linkBoxOneText: 'sprawdź',
         titleBoxTwo: 'Kierunek ekonomia, specjalność: Bankowość',
         imgTitleBoxTwo: "Bankowość",
-        textBoxTwo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea id molestias nostrum quidem rerum sequi suscipit veritatis? Cum iure, quam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea id molestias nostrum quidem rerum sequi suscipit veritatis? Cum iure, quam?',
+        textBoxTwo: 'Bankowość i ubezpieczenia to studia dla przyszłych pracowników banków i instytucji ubezpieczeniowych oraz osób chcących prowadzić własną działalność na rynku dystrybucji produktów bankowych i ubezpieczeniowych jako agenci czy brokerzy. Adresatem są też osoby, które cechuje kreatywność w pracy i umiejętności zarządzania ryzykiem oraz wysoka umiejętność nawiązywania kontaktów z odbiorcami produktów finansowych. Studenci tej specjalności zdobywają wiedzę z zakresu podstawowych produktów bankowych i ubezpieczeniowych, ryzyka w ich alokacji oraz organizacji i znaczenia sektora bankowego i ubezpieczeniowego w gospodarce i funkcjonowaniu podmiotów gospodarczych. Zajęcia prowadzone są przez doświadczonych wykładowców oraz w dużej części przez praktyków z bankowości i ubezpieczeń.',
+        linkBoxTwo: 'https://wsfi.edu.pl/files/bankowosc_i_ubezpieczenia_program.pdf',
+        linkBoxTwoTitle: 'Program studiów: ',
+        linkBoxTwoText: 'sprawdź',
         titleBoxThree: 'Kierunek ekonomia, specjalność: Przedsiębiorczość',
         imgTitleBoxThree: "Przedsiębiorczość",
-        textBoxThree: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea id molestias nostrum quidem rerum sequi suscipit veritatis? Cum iure, quam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea id molestias nostrum quidem rerum sequi suscipit veritatis? Cum iure, quam?'
+        textBoxThree: 'Przedsiębiorczość dostarcza studentom szeroką wiedzę pozwalającą zaprojektować i uruchomić własną firmę, a także realizować się na polu handlowca/sprzedawcy. WSFI w ostatnich latach współuczestniczyła w założeniu kilkuset mikroprzedsiębiorstw. Nasi wykładowcy i eksperci służyli radą i wsparciem na etapie rejestracji działalności, jak i pierwszych miesięcy funkcjonowania. Zdobytą wiedzą i doświadczeniem dzielimy się z naszymi studentami. W ramach specjalności Przedsiębiorczość studenci mogą wybrać jedną z dwóch ścieżek specjalizacyjnych: \n 1. Organizacja i rozwój firmy \n 2. Menedżer sprzedaży',
+        linkBoxThree: 'https://wsfi.edu.pl/files/przedsiebiorczosc_program.pdf',
+        linkBoxThreeTitle: 'Program studiów: ',
+        linkBoxThreeText: 'sprawdź',
     }
 
     const [slideData, setSlideData] = useState({
@@ -26,6 +35,9 @@ const Slider = () => {
             imgTitle: data.imgTitleBoxOne,
             title: data.titleBoxOne,
             text: data.textBoxOne,
+            link: data.linkBoxOne,
+            linkTitle: data.linkBoxOneTitle,
+            linkText: data.linkBoxOneText,
             box: e.target.id
         });
 
@@ -33,6 +45,9 @@ const Slider = () => {
             imgTitle: data.imgTitleBoxOne,
             title: data.titleBoxOne,
             text: data.textBoxOne,
+            link: data.linkBoxOne,
+            linkTitle: data.linkBoxOneTitle,
+            linkText: data.linkBoxOneText,
             box: e.target.id
         });
 
@@ -42,6 +57,9 @@ const Slider = () => {
             imgTitle: data.imgTitleBoxTwo,
             title: data.titleBoxTwo,
             text: data.textBoxTwo,
+            link: data.linkBoxTwo,
+            linkTitle: data.linkBoxTwoTitle,
+            linkText: data.linkBoxTwoText,
             box: e.target.id
         });
 
@@ -49,6 +67,9 @@ const Slider = () => {
             imgTitle: data.imgTitleBoxTwo,
             title: data.titleBoxTwo,
             text: data.textBoxTwo,
+            link: data.linkBoxTwo,
+            linkTitle: data.linkBoxTwoTitle,
+            linkText: data.linkBoxTwoText,
             box: e.target.id
         });
 
@@ -57,6 +78,9 @@ const Slider = () => {
             imgTitle: data.imgTitleBoxThree,
             title: data.titleBoxThree,
             text: data.textBoxThree,
+            link: data.linkBoxThree,
+            linkTitle: data.linkBoxThreeTitle,
+            linkText: data.linkBoxThreeText,
             box: e.target.id
         });
 
@@ -64,6 +88,9 @@ const Slider = () => {
             imgTitle: data.imgTitleBoxThree,
             title: data.titleBoxThree,
             text: data.textBoxThree,
+            link: data.linkBoxThree,
+            linkTitle: data.linkBoxThreeTitle,
+            linkText: data.linkBoxThreeText,
             box: e.target.id
         });
 
@@ -101,7 +128,6 @@ const Slider = () => {
 
 const SlideContent = ({slideData,onClick}) => {
 
-
     return(
         <div className='slide' onClick={onClick}>
             <div className='slide-content' id='slide' >
@@ -109,6 +135,10 @@ const SlideContent = ({slideData,onClick}) => {
                 <div>
                     <h3 className='slide-content__title'>{slideData.title}</h3>
                     <p className='slide-content__text'>{slideData.text}</p>
+                    <p className='slide-content__link-title'>{slideData.linkTitle}
+                        <a href={slideData.link} className='slide-content__link' target='_blank' rel='noopener noreferrer'>
+                            {slideData.linkText}</a>
+                    </p>
                 </div>
             </div>
         </div>
