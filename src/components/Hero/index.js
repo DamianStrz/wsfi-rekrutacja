8660bbce463265c3881dae294facfe635a7d7ada
DamianStrz <damian.strzalkowski@o2.pl>
2021-05-05 13:05:44 +0200

    Dalsze zmiany w wyglądzie.

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import heroBackground from '../../assets/hero-background-nobckrnd.png'
-import linkBackground from '../../assets/logo-wsfi-braz-bez-tła.png'
+import linkBackground from '../../assets/logo-wsfi-białe-bez-tła.png'
 
 const Hero = () => (
 
@@ -17,11 +17,12 @@ const Hero = () => (
         {/*<div className='hero-background'/>*/}
         <div className='hero-info'>
             <h1 className='hero-title'>Wyższa Szkoła Finansów i Informatyki - studia <span className='online'>on-line</span></h1>
-            <ul>E - learning to przyszłość na jaka zasługujesz! Przekonaj się już dziś, że to:
+            <ul className='hero-list'>
+                <span className='online'>E-learning</span> to przyszłość na jaka zasługujesz! Przekonaj się już dziś, że to:
                 <li>oszczędność czasu - nie musisz już stać w korkach w drodze na uczelnie</li>
                 <li>nie musisz stać w kolejce do dziekanatu czy na konsultacje - wszystkie
                     formalności możesz załatwiać za pomocą czatów, poczty oraz komunikatorów
-                    platform do e-learningu</li>
+                    platform do <span className='online'>e-learningu</span></li>
                 <li>sam decydujesz o tempie, czasie i miejscu nauki</li>
                 <li>poznasz nowe formy szybszej i efektywniejszej nauki - dzięki
                     innowacyjnemu podejściu do prezentowanych treści</li>
