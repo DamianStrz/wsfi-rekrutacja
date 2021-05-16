57f69658970622c1a834644db628b81648344435
DamianStrz <damian.strzalkowski@o2.pl>
2021-05-16 12:51:00 +0200

    Popup wyskakujący po odświeżeniu strony. Wymaga modyfikacji tłą.

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+
 const Popup = () => {
 
     const [isOpen, setIsOpen] = useState(true);
@@ -14,7 +15,9 @@ const Popup = () => {
                 <div className='popup'>
                     <div className='popup-box'>
                         <h1 className='popup-title'>Wejdź w świat studiów online</h1>
-                        <span className='popup-close' onClick={handleClose}>x</span>
+                        <div className='popup-close' onClick={handleClose}>
+                            <i className="fa fa-check"></i>
+                        </div>
                     </div>
                 </div>
             }
